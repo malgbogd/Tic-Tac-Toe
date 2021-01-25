f16e8fbd4e504a7d6c51e3e519cb2b14ad573889
Gosia <malgbogd@gmail.com>
2021-01-25 16:56:24 +0100

    Name buttons working

--- scripts.js
+++ scripts.js
@@ -1,14 +1,8 @@
-const createPlayers = (marker) => {
-    this.marker=marker;
-
-}
-
-const playerX = createPlayers("X");
-const playerO = createPlayers("O");
-
 const createBoard = (()=> {
 
     const DOMboard = document.getElementById("board");
+
+    let boardArray=[]
     
     for (let index = 0; index < 3; index++) {
     
@@ -17,19 +11,45 @@ const createBoard = (()=> {
         for (let index = 0; index < 3; index++) {
               square = document.createElement("div");
               square.className = "square"
-              square.id = "a" + index
+              boardArray[boardArray.length] = square;
               row.appendChild(square);
             }
 
         DOMboard.appendChild(row);      
     }
+    return boardArray;
 }
 )();
 
-const addPlayersNames = (()=>{
+const nameButtons = (()=> {
+
+function getXName() {
+    nameX = prompt("What is player X name?")
+    if (nameX.length == 0) {
+        buttonName1.innerHTML = "Name"
+    } else {
+        buttonName1.innerHTML = nameX
+    }
+}
+    
+const buttonName1 = document.getElementById("playerX")
+buttonName1.addEventListener("click", () => getXName());
+
+function getOName() {
+    nameO = prompt("What is player O name?")
+    if (nameO.length == 0) {
+        buttonName2.innerHTML = "Name"
+    } else {
+        buttonName2.innerHTML = nameO
+    }
+}
+
+const buttonName2 = document.getElementById("playerO")
+buttonName2.addEventListener("click", () => getOName());
 
 })();
 
-const playTheGame = (()=> {
-
-})();
+const Player = (marker) => {
+    const getMarker = () => marker
+    return getMarker;
+}
